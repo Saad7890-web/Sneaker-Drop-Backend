6a83c50bdcf80a0204d7217ef9ba722db5affe01
Saad7890-web <omysaadislam@gmail.com>
2026-06-12 20:11:04 +0600

    implement real time properly

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,27 @@ import http from "http";
 import app from "./app";
 import { env } from "./config/env";
 import logger from "./config/logger";
+import { startReservationExpirationWorker } from "./jobs/reservation-expiration.worker";
 import { initSocket } from "./socket";
 
 const server = http.createServer(app);
 
 initSocket(server);
+
+const stopWorker = startReservationExpirationWorker();
+
+const shutdown = (signal: NodeJS.Signals) => {
+  logger.info({ signal }, "Shutting down gracefully");
+
+  stopWorker();
+
+  server.close(() => {
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("SIGTERM", () => shutdown("SIGTERM"));
 
 server.listen(env.PORT, () => {
   logger.info(
