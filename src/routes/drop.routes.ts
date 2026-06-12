85810000a1aeee7593c49e13f04db7ab1de13cf0
Saad7890-web <omysaadislam@gmail.com>
2026-06-12 20:57:08 +0600

    backend done

--- src/routes/drop.routes.ts
+++ src/routes/drop.routes.ts
@@ -1,12 +1,36 @@
 import { Router } from "express";
 import { createDropController } from "../controllers/drop.controller";
+import {
+  getDropController,
+  listActiveDropsController,
+} from "../controllers/drop.query.controller";
 import { requireAuth } from "../middlewares/auth.middleware";
 import { requireRole } from "../middlewares/role.middleware";
-import { validateBody } from "../middlewares/validateRequest.middleware";
+import {
+  validateBody,
+  validateParams,
+  validateQuery,
+} from "../middlewares/validateRequest.middleware";
 import { asyncHandler } from "../utils/asyncHandler";
-import { createDropSchema } from "../validators/drop.validators";
+import { paginationQuerySchema } from "../validators/common.validators";
+import {
+  createDropSchema,
+  dropIdParamSchema,
+} from "../validators/drop.validators";
 
 const router = Router();
+
+router.get(
+  "/active",
+  validateQuery(paginationQuerySchema),
+  asyncHandler(listActiveDropsController),
+);
+
+router.get(
+  "/:dropId",
+  validateParams(dropIdParamSchema),
+  asyncHandler(getDropController),
+);
 
 router.post(
   "/",
